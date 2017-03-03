58c9d412f1c73a8331f67c0705b1772a04ce3fdc
Jorge Sierra <jsierra.siete@gmail.com>
2017-03-03 09:33:08 +0000

    Port change

--- app.js
+++ app.js
@@ -41,11 +41,11 @@ app.get('/user/:id?', function (req, res, next) {
   res.render('user.ejs', {user: username});
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port)
 
-})
+});
